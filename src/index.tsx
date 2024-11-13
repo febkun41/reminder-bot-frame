432dd980f6dfee5ad166f407bdf45071082bc6a6
benbaessler <mail@benbaessler.com>
2024-11-13 15:17:11 +0100

    feat: layout + flow

--- src/index.tsx
+++ src/index.tsx
@@ -11,17 +11,11 @@ export const app = new Frog({
 })
 
 app.frame('/', (c) => {
-  const { buttonValue, inputText, status } = c
-  const fruit = inputText || buttonValue
   return c.res({
     image: (
       <div
         style={{
           alignItems: 'center',
-          background:
-            status === 'response'
-              ? 'linear-gradient(to right, #432889, #17101F)'
-              : 'black',
           backgroundSize: '100% 100%',
           display: 'flex',
           flexDirection: 'column',
@@ -44,18 +38,61 @@ app.frame('/', (c) => {
             whiteSpace: 'pre-wrap',
           }}
         >
-          {status === 'response'
-            ? `Nice choice.${fruit ? ` ${fruit.toUpperCase()}!!` : ''}`
-            : 'Welcome!'}
+          Remind me of this cast
         </div>
       </div>
     ),
     intents: [
-      <TextInput placeholder="Enter custom fruit..." />,
-      <Button value="apples">Apples</Button>,
-      <Button value="oranges">Oranges</Button>,
-      <Button value="bananas">Bananas</Button>,
-      status === 'response' && <Button.Reset>Reset</Button.Reset>,
+      <TextInput placeholder="e.g. 1d 12h 25m" />,
+      <Button value="share">Share</Button>,
+      <Button action="/submit">Remind me</Button>,
+    ],
+  })
+})
+
+// @ts-ignore
+app.frame("/submit", (c) => {
+  const { inputText } = c
+  console.log(inputText);
+  if (!inputText) return c.error({
+    message: "Please input a time"
+  })
+
+  return c.res({
+    image: (
+      <div
+        style={{
+          alignItems: 'center',
+          backgroundSize: '100% 100%',
+          display: 'flex',
+          flexDirection: 'column',
+          flexWrap: 'nowrap',
+          height: '100%',
+          justifyContent: 'center',
+          textAlign: 'center',
+          width: '100%',
+        }}
+      >
+        <div
+          style={{
+            color: 'white',
+            fontSize: 60,
+            fontStyle: 'normal',
+            letterSpacing: '-0.025em',
+            lineHeight: 1.4,
+            marginTop: 30,
+            padding: '0 120px',
+            whiteSpace: 'pre-wrap',
+          }}
+        >
+          Good job
+        </div>
+      </div>
+    ),
+    intents: [
+      <TextInput placeholder="e.g. 1d 12h 25m" />,
+      <Button value="share">Share</Button>,
+      <Button value="submit">Remind me</Button>,
     ],
   })
 })
